04049fc488f1db315cc5d0e14c0c22658f313fa5
rob <robert.koehlmoos@gmail.com>
2021-09-06 13:27:54 -0400

    Project More Ready

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-# corporate said we need to add an exclamation mark to the end of this
+# corporate said we need to add an exclamation mark to the end of this message
 # apparently it helps with user engagement
 # Can you make that fix?
 print("Hello World")
+# Once you're done just commit and push the changes
